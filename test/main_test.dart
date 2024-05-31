715d7950a6429a770bf5f4d358c9fd0a97d3beb3
Rediet_W <vivezlaviequetuaimes@gmail.com>
2024-05-31 23:29:09 +0300

    user profile

--- test/main_test.dart
+++ test/main_test.dart
@@ -18,9 +18,6 @@ import 'package:flutter_test/flutter_test.dart';
 import './post/bloc/post_bloc_test.dart' as post_bloc_test;
 import './comment/bloc/comment_bloc_test.dart' as comment_bloc_test;
 import './auth/bloc/auth_bloc_test.dart' as auth_bloc_test;
-import './auth/repository/auth_repository_test.dart' as auth_repo_test;
-import 'comment/repository/comment_repository_test.dart' as comment_repo_test;
-import './post/repository/post_repository_test.dart' as post_repo_test;
 import './auth/model/auth_model_test.dart' as auth_model_test;
 import './comment/model/comment_model_test.dart' as comment_model_test;
 import './post/model/post_model_test.dart' as post_model_test;
@@ -34,10 +31,6 @@ void main() {
     post_bloc_test.main();
     comment_bloc_test.main();
     auth_bloc_test.main();
-    // call the tests for each repository
-    // auth_repo_test.main();
-    // comment_repo_test.main();
-    // post_repo_test.main();
     // call the tests for each provider
     auth_provider_test.main();
     comment_provider_test.main();
